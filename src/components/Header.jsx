b650aee8a34e7816534d721f8352d3e57181748d
moin <moin11.dev@gmail.com>
2024-08-06 21:45:49 +0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className='gap-4 p-4 w-full'>
+            <a href="/"><h1 className='text-center font-extrabold'>Moin-11 /<span className='text-blue-400 bold'>/ Visit My Github</span></h1></a>
+        
+        </header>
+    )
+}
